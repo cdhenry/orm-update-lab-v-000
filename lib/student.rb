45f4a744a64217623d56e6c368ee9d219732e3a3
Chris Henry <cdhenry@gmail.com>
2018-03-07 22:24:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,21 @@ class Student
     sql = <<-SQL
       DROP TABLE students
       SQL
-      
+
     DB[:conn].execute(sql)
   end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO songs (name, album) 
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.album)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
+  end
+  
 end
